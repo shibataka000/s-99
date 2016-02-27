3286bc1103c4630b073f3157c8a74b3d4b5d4495
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-28 00:00:41 +0900

    Add function to get primes.

--- src/main/scala/s99/arithmetic/S99Int.scala
+++ src/main/scala/s99/arithmetic/S99Int.scala
@@ -3,7 +3,7 @@ package s99.arithmetic
 class S99Int(val start: Int) {
   import S99Int._
 
-  def isPrime(): Boolean = start > 1 && (2 to (start - 1)).forall(start % _ != 0)
+  def isPrime(): Boolean = start > 1 && primes.takeWhile(_ < start).forall(start % _ != 0)
 
   def isCoprimeTo(n: Int): Boolean = gcd(start, n) == 1
 
@@ -12,6 +12,12 @@ class S99Int(val start: Int) {
 
 object S99Int {
   implicit def int2S99Int(i: Int): S99Int = new S99Int(i)
+
+  def primes(): Stream[Int] = {
+    def from(start: Int, step: Int = 1): Stream[Int] = start #:: from(start + step, step)
+    def helper(l: Stream[Int]): Stream[Int] = l.head #:: helper(l.filter(_ % l.head != 0))
+    2 #:: helper(from(3, 2))
+  }
 
   def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
 }
