dffd4cc34af9f23cc81510a6c77c744b7b73efaa
shibataka000 <chise.alter.pasta@gmail.com>
2016-02-28 16:19:17 +0900

    Solve P37.

--- src/main/scala/s99/arithmetic/S99Int.scala
+++ src/main/scala/s99/arithmetic/S99Int.scala
@@ -9,7 +9,9 @@ class S99Int(val start: Int) {
 
   def isCoprimeTo(n: Int): Boolean = gcd(start, n) == 1
 
-  def totient(): Int = (1 to start).count(this.isCoprimeTo(_))
+  def totient(): Int = start.primeFactorMultiplicity.map{
+    case (p, m) => (p - 1) * math.pow(p, m - 1).toInt
+  }.product
 
   def primeFactors(): List[Int] = {
     def helper(a: Int, ps: Stream[Int]): List[Int] = 
